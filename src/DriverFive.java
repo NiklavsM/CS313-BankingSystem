0507932d6dd6caa61bd5d8e7a8d0e1700c9848aa
gmb15146 <james.queen.2015@uni.strath.ac.uk>
2017-11-08 14:45:37 +0000

    Added a faulty way around multiple transfers.

---
+++ src/DriverFive.java
@@ -0,0 +1,32 @@
+public class DriverFive {
+    public static void main(String[] args) {
+
+        AccountManager accountManager = new AccountManager();
+        IAccount checkingAccount = new CheckingAccount("LV18HABAHA123231213");
+        IAccount checkingAccount2 = new CheckingAccount("OV18HABAHA123231213");
+        IAccount savingAccount = new SavingAccount("njisnjdfnjsndfjnj2434");
+        accountManager.addAccount(checkingAccount);
+        accountManager.addAccount(checkingAccount2);
+        accountManager.addAccount(savingAccount);
+        //######################### Checking ########################################
+        // Ordinary Tests for Checking
+        MakeDepositThread makeDepositThread1 = new MakeDepositThread(checkingAccount, 50);
+        MakeDepositThread makeDepositThread2 = new MakeDepositThread(checkingAccount2, 20);
+
+        TransferFundsThread transferFundsThread = new TransferFundsThread(checkingAccount,checkingAccount2,12);
+
+        MakeDepositThread makeDepositThread3 = new MakeDepositThread(checkingAccount, 5);
+
+        TransferFundsThread transferFundsThread2 = new TransferFundsThread(checkingAccount,checkingAccount2,4);
+        TransferFundsThread transferFundsThread3 = new TransferFundsThread(checkingAccount,checkingAccount2,5);
+
+        makeDepositThread1.start();
+        makeDepositThread2.start();
+        transferFundsThread.start();
+        makeDepositThread3.start();
+        transferFundsThread2.start();
+        transferFundsThread3.start();
+
+        //TODO: Differing results when more than 1 transfer
+    }
+}
--- src/TransferFundsThread.java
+++ src/TransferFundsThread.java
@@ -9,10 +9,29 @@ public class TransferFundsThread extends Thread {
         this.amount = amount;
     }
 
+    //FIXME: Probably a better way of doing this - only works for 2 transfers currently
     @Override
     public void run() {
+        Thread[] ts = new Thread[activeCount()];
+        boolean otherTransfer = false;
+        Thread tt = null;
+        if(enumerate(ts) > 0) {
+            for(Thread t : ts) {
+                if (t != null) {
+                    if(t.getClass().equals(this.getClass()) && t.getId() < this.getId() && t.getState() == State.RUNNABLE) {
+                        System.out.println("ANOTHER TRANSFER THREAD HAS BEEN FOUND!");
+                        otherTransfer = true;
+                        tt = t;
+                    }
+                }
+            }
+        }
         try {
-            accountSender.transferFunds(amount,accountReciever);
+            if(otherTransfer) {
+                tt.join();
+                System.out.println("Thread ID = " + this.getId() + " JUST JOINED WITH THREAD ID = " + tt.getId());
+            }
+            accountSender.transferFunds(amount, accountReciever);
         }
         catch (InterruptedException ex){
             System.out.println("Cant wait no longer - transfer unsuccessful");
